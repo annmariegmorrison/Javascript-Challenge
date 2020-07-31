1a5f0032b2776a4ec5ca5ca9bf6ae71d387c542c
AnnMarie Morrison <annmariemorrison@annmaries-mbp.home>
2020-07-31 15:18:45 -0500

    udpate

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -12,9 +12,34 @@ console.log(tableData);
 var row = tbody.append("tr");
 Object.entries(tableData).forEach(function([key, value]) {
 console.log(key, value);
-// Append a cell to the row for each value in the weather report object
+
+// Append a cell to the row for each value 
 var cell = row.append("td");
 cell.text(value);
 });
 });
-    
+ 
+var button = d3.select("#filter-btn");
+var form =  d3.select("#datetime");
+button.on("click", runEnter)
+form.on("submit", runEnter);
+
+// Listen for events 
+
+function runEnter() {
+    d3.event.preventDefault();
+    var date = d3.select("#datetime").property("value");
+    var filterData = tableData
+    if (date){
+        filterData = filterData.filter(row => row.datetime === date);
+        console.log(filterData);
+    }
+    tbody.html("")
+    filterData.forEach((ufoSightings) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSightings).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    }); 
+}
